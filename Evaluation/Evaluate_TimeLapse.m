343f9b80821a11b0bf879275aef6d66ee62deb79
wsl0168 <wsl0168@gmail.com>
2018-06-21 15:57:22 -0400

    Fix Evaluation

--- Evaluation/Evaluate_TimeLapse.m
+++ Evaluation/Evaluate_TimeLapse.m
@@ -1,26 +1,36 @@
-clear all;
-clc
-close all;
+% clear all;
+% clc
+% close all;
 ppm = 2;
 addpath('NeuronTracerV20');
-addpath('C:\Users\Seyed\Documents\DatasetTests\GUI\NCT_Registration\Functions');
+% GTpath = 'E:\Datasets\TimeLaps\';
+% GTpath = E:\Shih-Luen\Lab\Projects\RegistrationEvaluation\TimeLaps\';
+% Functionspath = 'E:\Shih-Luen\Lab\Projects\registrar\Functions\';
+% resultpath = 'C:\Users\Seyed\Documents\DatasetTests\GUI\NCT_Registration\MicroscopeFiles\Results-TimeLapse_Holtmaat_StackList\';
+
+
+GTpath = 'E:\Shih-Luen\Lab\Projects\RegistrationEvaluation\TimeLaps\';
+Functionspath = 'E:\Shih-Luen\Lab\Projects\registrar\';
+resultpath = 'E:\Shih-Luen\Lab\Projects\RegistrationEvaluation\Results-TimeLapse_Holtmaat_StackList4\';
+
+addpath([Functionspath,'Functions']);
 % load('../data/StackData_TimeLapse_Holtmaat.mat');
-load('E:\Datasets\TimeLaps\Matches\DL083-001-Matches.mat');
-useTrace = 1;
+load([GTpath,'Matches\DL083-001-Matches.mat']);
+useTrace = 0; % 0 = use boutons 1 = use trace
 showTranceonImage = 0;
 
-fname_First = dir('E:\Datasets\TimeLaps\Matches\Traces\DL083C001-A0*');
-fname_First={fname_First.name}';
-fname_Second = dir('E:\Datasets\TimeLaps\Matches\Traces\DL083D001-A0*');
-fname_Second={fname_Second.name}';
-AllDistances = zeros(size(fname_First,1),6);
-pixelSize = [0.26 0.26 0.8];
+T_Names = {'B','C','D','E','F','G','H','I','J','K','L','M','N'};
 
 % Show Traces
 sourceID = 1;
 targetID = 2;
 
-
+fname_First = dir([GTpath,'Matches\Traces\DL083',T_Names{sourceID},'001-A0*']);
+fname_First={fname_First.name}';
+fname_Second = dir([GTpath,'Matches\Traces\DL083',T_Names{targetID},'001-A0*']);
+fname_Second={fname_Second.name}';
+AllDistances = zeros(size(fname_First,1),6);
+pixelSize = [0.26 0.26 0.8];
 %     Source_Stack_File = [char(StackList(sourceID,2)),'\',char(StackList(sourceID,1)),'.tif'];
 %         Target_Stack_File = [char(StackList(targetID,2)),'\',char(StackList(targetID,1)),'.tif'];
 %         IM_Source=ImportStack(char(Source_Stack_File));
@@ -51,8 +61,8 @@ targetID = 2;
 for i=1:size(fname_First,1)
     if useTrace
         % Using Trace
-        sourcePath = ['E:\Datasets\TimeLaps\Matches\Traces\',fname_First{i}];%'E:\Datasets\TimeLaps\Matches\Traces\DL083B001-A001.swc';
-        targetPath = ['E:\Datasets\TimeLaps\Matches\Traces\',fname_Second{i}];%'E:\Datasets\TimeLaps\Matches\Traces\DL083C001-A001.swc';
+        sourcePath = [GTpath,'Matches\Traces\',fname_First{i}];%'E:\Datasets\TimeLaps\Matches\Traces\DL083B001-A001.swc';
+        targetPath = [GTpath,'Matches\Traces\',fname_Second{i}];%'E:\Datasets\TimeLaps\Matches\Traces\DL083C001-A001.swc';
         %         sourcePath = 'E:\Datasets\TimeLaps\Matches\Traces\DL083B001-A001.swc';
         %         targetPath = 'E:\Datasets\TimeLaps\Matches\Traces\DL083C001-A001.swc';
         
@@ -63,14 +73,17 @@ for i=1:size(fname_First,1)
         SourcePoints = r_Source;
         TargetPoints = r_Target;
     else
-        SourcePoints = Matches.BC.r1;
-        TargetPoints = Matches.BC.r2;
+        %Use Boutons
+        Boutons = struct2cell(Matches);
+        B1 = Boutons{sourceID,1};
+        SourcePoints = B1.r2;
+        TargetPoints = B1.r1; 
     end
     
     
     
-    sourceID = 1;
-    targetID = 2;
+%     sourceID = 1;
+%     targetID = 2;
     
     % Source_StackPositions = StackPositions_pixels(sourceID,:);
     % Target_StackPositions = StackPositions_pixels(targetID,:);
@@ -99,113 +112,155 @@ for i=1:size(fname_First,1)
     %         axis equal
     %         PlotAM(AM_Target,TargetPoints,'g')
     %         axis equal
-        if useTrace
-            [Distances_Before,~] = TraceDistance(AM_Source, SourcePoints, AM_Target, TargetPoints,pixelSize,0);
-            D_Before = mean(Distances_Before)
-        else
-            D_Before = mean(mean((SourcePoints-TargetPoints).^2,1).^0.5)
-        end
+    if useTrace
+        [Dis_Before_um,Dis_Before_voxel] = TraceDistance(AM_Source, SourcePoints, AM_Target, TargetPoints,pixelSize,0);
+        D_Before_um = mean(Dis_Before_um);
+        D_Before_voxel = mean(Dis_Before_voxel);
+    else
+        D_Before_voxel = mean(mean((SourcePoints-TargetPoints).^2,1).^0.5);
+    end
     
     %     Dag_temp=(bsxfun(@minus,SourcePoints(:,1),TargetPoints(:,1)').^2+bsxfun(@minus,SourcePoints(:,2),TargetPoints(:,2)').^2+bsxfun(@minus,SourcePoints(:,3),TargetPoints(:,3)').^2).^0.5;
     %     [AM,~]=Hungarian_fast(Dag_temp);
     %     [D_Before,~] = TraceDistance(AM, SourcePoints, AM, TargetPoints,0)
     
     
-    FeaturePositions_T = load('C:\Users\Seyed\Documents\DatasetTests\GUI\NCT_Registration\MicroscopeFiles\Results-TimeLapse_Holtmaat_StackList\MatchedPoints_Translation.mat');
-    FeaturePositions_R = load('C:\Users\Seyed\Documents\DatasetTests\GUI\NCT_Registration\MicroscopeFiles\Results-TimeLapse_Holtmaat_StackList\MatchedPoints_Rigid.mat');
-    FeaturePositions_A = load('C:\Users\Seyed\Documents\DatasetTests\GUI\NCT_Registration\MicroscopeFiles\Results-TimeLapse_Holtmaat_StackList\MatchedPoints_Affine.mat');
-    FeaturePositions_NR = load('C:\Users\Seyed\Documents\DatasetTests\GUI\NCT_Registration\MicroscopeFiles\Results-TimeLapse_Holtmaat_StackList\MatchedPoints_Non-Rigid.mat');
+    %     FeaturePositions_T = load([resultpath,'MatchedPoints_Translation.mat']);
+    %     FeaturePositions_R = load([resultpath,'MatchedPoints_Rigid.mat']);
+    %     FeaturePositions_A = load([resultpath,'MatchedPoints_Affine.mat']);
+    FeaturePositions_NR = load([resultpath,'MatchedPoints_Non-Rigid.mat']);
     
     
     
-    % Trnslation
-    Global_Matched_Source = FeaturePositions_T.Matched{sourceID,targetID}(:,1:3)';
-    Global_Matched_Target = FeaturePositions_T.Matched{sourceID,targetID}(:,4:6)';
-    b=Optimal_Translation_Transform(Global_Matched_Source,Global_Matched_Target);
+    %     % Trnslation
+    %     Global_Matched_Source = FeaturePositions_T.Matched{sourceID,targetID}(:,1:3)';
+    %     Global_Matched_Target = FeaturePositions_T.Matched{sourceID,targetID}(:,4:6)';
+    %     b=Optimal_Translation_Transform(Global_Matched_Source,Global_Matched_Target);
+    %
+    %     SourcePoints_TranslationPairs = SourcePoints+b';
+    %     TargetPoints_TranslationPairs = TargetPoints;
+    %
+    %     if useTrace
+    %         [Distances_Translation,~] = TraceDistance(AM_Source, SourcePoints_TranslationPairs, AM_Target, TargetPoints_TranslationPairs,pixelSize,0);
+    %         D_Translation = mean(Distances_Translation)
+    %     else
+    %         D_Translation = mean(mean((SourcePoints_TranslationPairs-TargetPoints_TranslationPairs).^2,1).^0.5)
+    %     end
+    %
+    %     % Rigid
+    %     Global_Matched_Source = FeaturePositions_R.Matched{sourceID,targetID}(:,1:3)';
+    %     Global_Matched_Target = FeaturePositions_R.Matched{sourceID,targetID}(:,4:6)';
+    %     [R,b]=Optimal_Rigid_Transform(Global_Matched_Source,Global_Matched_Target);
+    %
+    %     SourcePoints_Rigid = (R*(SourcePoints)'+b)';
+    %     TargetPoints_Rigid = TargetPoints;
+    %     if useTrace
+    %         [Distances_Rigid,~] = TraceDistance(AM_Source, SourcePoints_Rigid, AM_Target, TargetPoints_Rigid,pixelSize,0);
+    %         D_Rigid = mean(Distances_Rigid)
+    %     else
+    %         D_Rigid = mean(mean((SourcePoints_Rigid-TargetPoints_Rigid).^2,1).^0.5)
+    %     end
+    %
+    %     % Affine
+    %     Global_Matched_Source = FeaturePositions_A.Matched{sourceID,targetID}(:,1:3)';
+    %     Global_Matched_Target = FeaturePositions_A.Matched{sourceID,targetID}(:,4:6)';
+    %     [L,b]=Optimal_Affine_Transform(Global_Matched_Source,Global_Matched_Target);
+    %
+    %     SourcePoints_Affine = (L*(SourcePoints)'+b)';
+    %     TargetPoints_Affine = TargetPoints;
+    %     if useTrace
+    %         [Distances_Affine,~] = TraceDistance(AM_Source, SourcePoints_Affine, AM_Target, TargetPoints_Affine,pixelSize,0);
+    %         D_Affine = mean(Distances_Affine)
+    %     else
+    %         D_Affine = mean(mean((SourcePoints_Affine-TargetPoints_Affine).^2,1).^0.5)
+    %     end
+    %
+    %
+    %
+    %     % AffineNonRigid
+    %     Global_Matched_Source = FeaturePositions_A.Matched{sourceID,targetID}(:,1:3)';
+    %     Global_Matched_Target = FeaturePositions_A.Matched{sourceID,targetID}(:,4:6)';
+    %     [L,b]=Optimal_Affine_Transform(Global_Matched_Source,Global_Matched_Target);
+    %
+    %     SourcePoints_Affine = (L*(SourcePoints)'+b)';
+    %     TargetPoints_Affine = TargetPoints;
+    %
+    %     N_L=3;
+    %     temp = (L*Global_Matched_Source+b);
+    %     Min=min([min(temp,[],2),min(Global_Matched_Target,[],2)],[],2);
+    %     Max=max([max(temp,[],2),max(Global_Matched_Target,[],2)],[],2);
+    %     [~,XYZlmn,N_L,Min,Max]=Optimal_Nonrigid_Transform(temp,Global_Matched_Target,N_L,Min,Max);
+    %     SourcePoints_Affine=Perform_Nonrigid_Transform(SourcePoints_Affine',XYZlmn,N_L,Min,Max)';
+    %     if useTrace
+    %         [Distances_NonRigid,~] = TraceDistance(AM_Source, SourcePoints_Affine, AM_Target, TargetPoints_Affine,pixelSize,0);
+    %         D_NonRigidAffine = mean(Distances_NonRigid)
+    %     else
+    %         D_NonRigidAffine = mean(mean((SourcePoints_Affine-TargetPoints_Affine).^2,1).^0.5)
+    %     end
     
-    SourcePoints_TranslationPairs = SourcePoints+b';
-    TargetPoints_TranslationPairs = TargetPoints;
     
+    
+    %     % NonRigid
+    %     Global_Matched_Source = FeaturePositions_NR.Matched{sourceID,targetID}(:,1:3)';
+    %     Global_Matched_Target = FeaturePositions_NR.Matched{sourceID,targetID}(:,4:6)';
+    %     N_L=3;
+    %     Min=min([min(Global_Matched_Source,[],2),min(Global_Matched_Target,[],2)],[],2);
+    %     Max=max([max(Global_Matched_Source,[],2),max(Global_Matched_Target,[],2)],[],2);
+    %     [~,XYZlmn,N_L,Min,Max]=Optimal_Nonrigid_Transform(Global_Matched_Source,Global_Matched_Target,N_L,Min,Max);
+    %     SourcePoints_NR=Perform_Nonrigid_Transform(SourcePoints',XYZlmn,N_L,Min,Max);
+    %     if useTrace
+    %         [Distances_NonRigid,~] = TraceDistance(AM_Source, SourcePoints_NR', AM_Target, TargetPoints,pixelSize,0);
+    %         D_NonRigid = mean(Distances_NonRigid);
+    %     else
+    %         D_NonRigid = mean(mean((SourcePoints_NR-Global_Matched_Target).^2,1).^0.5)
+    %     end
+    %     disp('-------------------------------------');
+    
+    % B-Spline-NonRigid
+    Global_Matched_Source = FeaturePositions_NR.Matched{sourceID,targetID}(:,4:6)';
+    Global_Matched_Target = FeaturePositions_NR.Matched{sourceID,targetID}(:,1:3)';
+%     N_L=3;
+%     Min=min([min(Global_Matched_Source,[],2),min(Global_Matched_Target,[],2)],[],2);
+%     Max=max([max(Global_Matched_Source,[],2),max(Global_Matched_Target,[],2)],[],2);
+%     [~,XYZlmn,N_L,Min,Max]=Optimal_Nonrigid_Transform(Global_Matched_Source,Global_Matched_Target,N_L,Min,Max);
+%     SourcePoints_NR=Perform_Nonrigid_Transform(SourcePoints',XYZlmn,N_L,Min,Max);
+    Minimum = min(Global_Matched_Source,[],2);
+    Maximum = max(Global_Matched_Source,[],2);
+    nxyz = [1024,1024,312]; %image size
+    Nxyz = ceil((Maximum-Minimum)./nxyz');
+    [~,Cxyz]=Optimal_Bspline_Transform(Global_Matched_Source,Global_Matched_Target,Nxyz,nxyz);
+    [SourcePoints_NR,~]=Perform_Bspline_Transform(SourcePoints',Cxyz,Nxyz,nxyz);
     if useTrace
-        [Distances_Translation,~] = TraceDistance(AM_Source, SourcePoints_TranslationPairs, AM_Target, TargetPoints_TranslationPairs,pixelSize,0);
-        D_Translation = mean(Distances_Translation)
+        [Dis_NonRigid_um,Dis_NonRigid_voxel] = TraceDistance(AM_Source, SourcePoints_NR', AM_Target, TargetPoints,pixelSize,0);
+        D_NonRigid_um = mean(Dis_NonRigid_um);
+        D_NonRigid_voxel = mean(Dis_NonRigid_voxel);
     else
-        D_Translation = mean(mean((SourcePoints_TranslationPairs-TargetPoints_TranslationPairs).^2,1).^0.5)
-    end
-    
-    % Rigid
-    Global_Matched_Source = FeaturePositions_R.Matched{sourceID,targetID}(:,1:3)';
-    Global_Matched_Target = FeaturePositions_R.Matched{sourceID,targetID}(:,4:6)';
-    [R,b]=Optimal_Rigid_Transform(Global_Matched_Source,Global_Matched_Target);
-    
-    SourcePoints_Rigid = (R*(SourcePoints)'+b)';
-    TargetPoints_Rigid = TargetPoints;
-    if useTrace
-        [Distances_Rigid,~] = TraceDistance(AM_Source, SourcePoints_Rigid, AM_Target, TargetPoints_Rigid,pixelSize,0);
-        D_Rigid = mean(Distances_Rigid)
-    else
-        D_Rigid = mean(mean((SourcePoints_Rigid-TargetPoints_Rigid).^2,1).^0.5)
-    end
-    
-    % Affine
-    Global_Matched_Source = FeaturePositions_A.Matched{sourceID,targetID}(:,1:3)';
-    Global_Matched_Target = FeaturePositions_A.Matched{sourceID,targetID}(:,4:6)';
-    [L,b]=Optimal_Affine_Transform(Global_Matched_Source,Global_Matched_Target);
-    
-    SourcePoints_Affine = (L*(SourcePoints)'+b)';
-    TargetPoints_Affine = TargetPoints;
-    if useTrace
-        [Distances_Affine,~] = TraceDistance(AM_Source, SourcePoints_Affine, AM_Target, TargetPoints_Affine,pixelSize,0);
-        D_Affine = mean(Distances_Affine)
-    else
-        D_Affine = mean(mean((SourcePoints_Affine-TargetPoints_Affine).^2,1).^0.5)
-    end
-    
-    
-    
-    % AffineNonRigid
-    Global_Matched_Source = FeaturePositions_A.Matched{sourceID,targetID}(:,1:3)';
-    Global_Matched_Target = FeaturePositions_A.Matched{sourceID,targetID}(:,4:6)';
-    [L,b]=Optimal_Affine_Transform(Global_Matched_Source,Global_Matched_Target);
-    
-    SourcePoints_Affine = (L*(SourcePoints)'+b)';
-    TargetPoints_Affine = TargetPoints;
-    
-    N_L=3;
-    temp = (L*Global_Matched_Source+b);
-    Min=min([min(temp,[],2),min(Global_Matched_Target,[],2)],[],2);
-    Max=max([max(temp,[],2),max(Global_Matched_Target,[],2)],[],2);
-    [~,XYZlmn,N_L,Min,Max]=Optimal_Nonrigid_Transform(temp,Global_Matched_Target,N_L,Min,Max);
-    SourcePoints_Affine=Perform_Nonrigid_Transform(SourcePoints_Affine',XYZlmn,N_L,Min,Max)';
-    if useTrace
-        [Distances_NonRigid,~] = TraceDistance(AM_Source, SourcePoints_Affine, AM_Target, TargetPoints_Affine,pixelSize,0);
-        D_NonRigidAffine = mean(Distances_NonRigid)
-    else
-        D_NonRigidAffine = mean(mean((SourcePoints_Affine-TargetPoints_Affine).^2,1).^0.5)
-    end
-    
-    
-    
-    % NonRigid
-    Global_Matched_Source = FeaturePositions_NR.Matched{sourceID,targetID}(:,1:3)';
-    Global_Matched_Target = FeaturePositions_NR.Matched{sourceID,targetID}(:,4:6)';
-    N_L=3;
-    Min=min([min(Global_Matched_Source,[],2),min(Global_Matched_Target,[],2)],[],2);
-    Max=max([max(Global_Matched_Source,[],2),max(Global_Matched_Target,[],2)],[],2);
-    [~,XYZlmn,N_L,Min,Max]=Optimal_Nonrigid_Transform(Global_Matched_Source,Global_Matched_Target,N_L,Min,Max);
-    SourcePoints_NR=Perform_Nonrigid_Transform(SourcePoints',XYZlmn,N_L,Min,Max);
-    if useTrace
-        [Distances_NonRigid,~] = TraceDistance(AM_Source, SourcePoints_NR', AM_Target, TargetPoints,pixelSize,0);
-        D_NonRigid = mean(Distances_NonRigid);
-    else
-        D_NonRigid = mean(mean((SourcePoints_NR-Global_Matched_Target).^2,1).^0.5)
+        D_NonRigid_voxel = mean(mean((SourcePoints_NR'-TargetPoints).^2,1).^0.5);
     end
     disp('-------------------------------------');
     
-    AllDistances(i,1) = D_Before;
-    AllDistances(i,2) = D_Translation;
-    AllDistances(i,3) = D_Rigid;
-    AllDistances(i,4) = D_Affine;
-    AllDistances(i,5) = D_NonRigidAffine;
-    AllDistances(i,6) = D_NonRigid;
+    D_Before_voxel
+    D_NonRigid_voxel
+    
+%     D_Before_um
+%     D_NonRigid_um
+    
+    
+%     AllDistances_um(i,1) = D_Before_um;
+%     AllDistances_um(i,2) = D_Translation_um;
+%     AllDistances_um(i,3) = D_Rigid_um;
+%     AllDistances_um(i,4) = D_Affine_um;
+%     AllDistances_um(i,5) = D_NonRigidAffine_um;
+%     AllDistances_um(i,6) = D_NonRigid_um;
+    
+      AllDistances_voxel(i,1) = D_Before_voxel;
+%     AllDistances_um(i,2) = D_Translation_um;
+%     AllDistances_um(i,3) = D_Rigid_um;
+%     AllDistances_um(i,4) = D_Affine_um;
+%     AllDistances_um(i,5) = D_NonRigidAffine_um;
+    AllDistances_voxel(i,6) = D_NonRigid_voxel;
+    
+    if ~useTrace
+        break;
+    end
 end
