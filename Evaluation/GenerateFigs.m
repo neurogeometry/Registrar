f4f2de2e66e12762ae030c01745d9226b7b469a5
mousavikahaki <mousavikahaki@gmail.com>
2018-07-18 20:13:20 -0400

    Figure

--- Evaluation/GenerateFigs.m
+++ Evaluation/GenerateFigs.m
@@ -10,14 +10,14 @@ affine = 1;
 ppm = 1;
 N = 1; %Image Numbers
 addpath('NeuronTracerV20');
-csvPath = 'TimeLapse_Holtmaat_StackList.csv';
+csvPath = '..\..\RegistrationEvaluation\TimeLapse_Holtmaat_StackList.csv';
 StackList = table2cell(readtable(csvPath,'Delimiter',','));
 GTpath = '..\..\RegistrationEvaluation\TimeLaps\';
 Functionspath = '..\';
-% resultpath = '..\..\RegistrationEvaluation\Results-TimeLapse_Holtmaat_StackList\';
+resultpath = '..\..\RegistrationEvaluation\Results-TimeLapse_Holtmaat_StackList\';
 addpath([Functionspath,'Functions']);
 % Laod all matched Boutons
-load('DL083-001-Matches.mat');
+load([GTpath,'Matches\DL083-001-Matches.mat']);
 
 
 T_Names = {'B','C','D','E','F','G','H','I','J','K','L','M','N'};
@@ -60,14 +60,14 @@ fname_Second={fname_Second.name}';
 color = rand(size(fname_First,1),3);
 
 % Laod images
-% Source_Stack_File = char(StackList(sourceID,1));
-% Target_Stack_File = char(StackList(targetID,1));
-% IM_Source=ImportStack(char(Source_Stack_File));
-% IM_Source = uint8(double(IM_Source)./double(max(IM_Source(:))).*255);
-% IM_Target=ImportStack(char(Target_Stack_File));
-% IM_Target = uint8(double(IM_Target)./double(max(IM_Target(:))).*255);
-% IM_source_max=max(IM_Source,[],3);
-% IM_target_max=max(IM_Target,[],3);
+Source_Stack_File = char(StackList(sourceID,1));
+Target_Stack_File = char(StackList(targetID,1));
+IM_Source=ImportStack(char(Source_Stack_File));
+IM_Source = uint8(double(IM_Source)./double(max(IM_Source(:))).*255);
+IM_Target=ImportStack(char(Target_Stack_File));
+IM_Target = uint8(double(IM_Target)./double(max(IM_Target(:))).*255);
+IM_source_max=max(IM_Source,[],3);
+IM_target_max=max(IM_Target,[],3);
 
 %% Show All Traces  ----------------- Fig 1. A
 % N = N + 1;
@@ -333,9 +333,10 @@ color = rand(size(fname_First,1),3);
 % end
 
 %                     ----------------- Fig 4.B
-% IMAll = uint8(zeros(1024,1024));
-TR = -600;
-for ID = 1: 2%size(StackList,1)
+IMAll = uint8(zeros(1024,1024));
+TR = 0;
+AddSpace = 40;
+for ID = 1: size(StackList,1)
     sourceID = ID;
     targetID = ID + 1;
     fname = dir([GTpath,'Matches\Traces\DL083',T_Names{sourceID},'001-A0*']);
@@ -344,81 +345,78 @@ for ID = 1: 2%size(StackList,1)
     [AM_source,r_source,R]=swc2AM(Path);
     [AM_source,r_source,~] = AdjustPPM(AM_source,r_source,R,ppm);
     
-    fname = dir([GTpath,'Matches\Traces\DL083',T_Names{targetID},'001-A0*']);
-    fname={fname.name}';
-    Path = [GTpath,'Matches\Traces\',fname{TraceNum}];
-    [AM_target,r_target,R]=swc2AM(Path);
-    [AM_target,r_target,~] = AdjustPPM(AM_target,r_target,R,ppm);
-    
-%     Stack_File = char(StackList(sourceID,1));
-%     IM=ImportStack(char(Stack_File));
-%     IM = uint8(double(IM)./double(max(IM(:))).*255);
-%     IM_max=max(IM,[],3);
-%     
-%     [KT]=FastMarchingTube(size(IM),r,3,[1,1,1]);
-%     IMmax =  max(uint8(KT).*IM,[],3);
-%     IMmax = imtranslate(IMmax,[200, 0]);
-    
-    TR = TR + 50;
-%     IMmax = imtranslate(IMmax,[0, TR]);
-    
-%     IMAll = max(IMAll,IMmax);
-%     figure(N);imshow(IMAll,[0 20])
     
     
-    if sourceID == size(StackList,1)
-        B1 = Boutons{sourceID-1,1};
-        SourcePoints = B1.r2;
+        Stack_File = char(StackList(sourceID,1));
+        IM=ImportStack(char(Stack_File));
+        IM = uint8(double(IM)./double(max(IM(:))).*255);
+        IM_max=max(IM,[],3);
+    
+        [KT]=FastMarchingTube(size(IM),r_source,3,[1,1,1]);
+        IMmax =  max(uint8(KT).*IM,[],3);
+        IMmax = imtranslate(IMmax,[0, TR]);
+        IMAll = max(IMAll,IMmax);
+    
+    if ID < size(StackList,1)
         
-    else
+        fname = dir([GTpath,'Matches\Traces\DL083',T_Names{targetID},'001-A0*']);
+        fname={fname.name}';
+        Path = [GTpath,'Matches\Traces\',fname{TraceNum}];
+        [AM_target,r_target,R]=swc2AM(Path);
+        [AM_target,r_target,~] = AdjustPPM(AM_target,r_target,R,ppm);
+        
         B1 = Boutons{sourceID,1};
         SourcePoints = B1.r1;
         TargetPoints = B1.r2;
         
-    end
-    
-    [k,f] = dsearchn(r_source,SourcePoints);
-    r_source(:,2) = r_source(:,2) + 200;
-    r_source(:,1) = r_source(:,1) + TR;
-    figure(N); hold on; PlotAM(AM_source,r_source,color(TraceNum+sourceID-1,:))
-    SourcePoints(:,1) = SourcePoints(:,1)+TR;
-    SourcePoints(:,2) = SourcePoints(:,2) + 200;
-    s = 1;
-    for j=1:size(k)
-        if f(j)< 3
-            figure(N);hold on; plot(SourcePoints(j,2),SourcePoints(j,1),'or')
-            SourcePoints_onTrace(s,:) = SourcePoints(j,:);
-            s = s +1;
-        end
-    end
-    
-    [k,f] = dsearchn(r_target,TargetPoints);
-    r_target(:,2) = r_target(:,2) + 200;
-    r_target(:,1) = r_target(:,1) + TR;
-    figure(N); hold on; PlotAM(AM_target,r_target,color(TraceNum+sourceID-1,:))
-    TargetPoints(:,1) = TargetPoints(:,1)+TR;
-    TargetPoints(:,2) = TargetPoints(:,2) + 200;
-    s = 1;
-    for j=1:size(k)
-        if f(j)< 3
-%             figure(N);hold on; plot(TargetPoints(j,2),TargetPoints(j,1),'or')
-            TargetPoints_onTrace(s,:) = TargetPoints(j,:);
-            s = s +1;
-        end
-    end
-    
-    
-    for i=1:size(SourcePoints_onTrace,1)
-        figure(N);hold on;line([SourcePoints_onTrace(i,2) TargetPoints_onTrace(i,2)], ...
-            [SourcePoints_onTrace(i,1) TargetPoints_onTrace(i,1)], 'Color', rand(1,3));
         
+        [k,f] = dsearchn(r_source,SourcePoints);
+        
+        s = 1;
+        SourcePoints_onTrace=[];
+        for j=1:size(k)
+            if f(j)< 3
+                SourcePoints_onTrace(s,:) = SourcePoints(j,:);
+                s = s +1;
+            end
+        end
+        figure(N); hold on;
+        %     if ID==1
+        %     figure(N); hold on; PlotAM(AM_source,r_source,'c')
+        %
+        %     end
+        
+        plot3(SourcePoints_onTrace(:,2),SourcePoints_onTrace(:,1)+TR,ones(1,length(SourcePoints_onTrace(:,2))),'or')
+        
+        TR = TR - AddSpace;
+        [k,f] = dsearchn(r_target,TargetPoints);
+        TargetPoints(:,1) = TargetPoints(:,1)+TR;
+        r_target(:,1)=r_target(:,1)+TR;
+        %     PlotAM(AM_target,r_target,'c')
+        s = 1;
+        TargetPoints_onTrace=[];
+        for j=1:size(k)
+            if f(j)< 3
+                TargetPoints_onTrace(s,:) = TargetPoints(j,:);
+                s = s +1;
+            end
+        end
+        plot3(TargetPoints_onTrace(:,2),TargetPoints_onTrace(:,1),ones(1,length(TargetPoints_onTrace(:,2))),'or')
+        
+        if ID==1
+            line([SourcePoints_onTrace(:,2) TargetPoints_onTrace(:,2)]', ...
+                [SourcePoints_onTrace(:,1) TargetPoints_onTrace(:,1)]',[1;1]*ones(1,length(TargetPoints_onTrace(:,1))), 'Color', 'c');
+        else
+            line([SourcePoints_onTrace(:,2) TargetPoints_onTrace(:,2)]', ...
+                [SourcePoints_onTrace(:,1)+TR+AddSpace TargetPoints_onTrace(:,1)]',[1;1]*ones(1,length(TargetPoints_onTrace(:,1))), 'Color', 'c');
+        end
+        drawnow
     end
-%     axis equal
     
-    
-    
-       
 end
+
+
+    figure(N);imshow(IMAll,[0 20])
 
 % openfig('E:\Fig4B.fig');
 
